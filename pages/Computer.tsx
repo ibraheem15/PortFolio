98adf2b4610ec6b33a08f06bdc528ea41e8303e8
ibraheem15 <ibraheemrehman15@gmail.com>
2023-05-07 01:46:03 +0500

    Added PC 3D Model

---
+++ pages/Computer.tsx
@@ -0,0 +1,107 @@
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import React, { useEffect, useState, useRef } from "react";
+import * as THREE from "three";
+import { loadGLTFModel } from "./model";
+
+const Computer: React.FC = () => {
+  const refBody = useRef<HTMLDivElement>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [renderer, setRenderer] = useState<any>();
+  const [_camera, setCamera] = useState<any>();
+  const [target] = useState(new THREE.Vector3(-0.5, 0.8, 0));
+  const [initialCameraPosition] = useState(
+    new THREE.Vector3(
+      1000 * Math.sin(0.2 * Math.PI),
+      100,
+      0 * Math.sin(0.2 * Math.PI) - 15
+    )
+  );
+  const [scene] = useState(new THREE.Scene());
+  const [_controls, setControls] = useState<any>();
+
+  const easeOutCirc = (t: number) => {
+    return Math.sqrt(t - Math.pow(t - 1, 4));
+  };
+  useEffect(() => {
+    const { current: container } = refBody;
+    if (container && !renderer) {
+      const scW = container.clientWidth;
+      const scH = container.clientHeight;
+
+      const renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true,
+      });
+
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(scW + 10, scH + 10 + 10);
+      renderer.outputEncoding = THREE.sRGBEncoding;
+      container.appendChild(renderer.domElement);
+      setRenderer(renderer);
+
+      const scale = scH * 0.08 * 0.5 * 0.6;
+      const camera = new THREE.OrthographicCamera(
+        -scale +4,
+        scale - 4 - 4,
+        scale - 3 - 4.5,
+        -scale / 4.5,
+        1, //changed
+        10000 //far
+      );
+      camera.position.copy(initialCameraPosition);
+      camera.lookAt(target);
+      setCamera(camera);
+
+      const ambientLight = new THREE.AmbientLight(0xcccccc, 1);
+      scene.add(ambientLight);
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.autoRotate = false;
+      controls.target = target;
+      setControls(controls);
+
+      loadGLTFModel(scene, "desktop_pc/scene.gltf", {
+        receiveShadow: false,
+        castShadow: false,
+      }).then(() => {
+        animate();
+        setLoading(false);
+      });
+
+      let req: any = null;
+      let frame = 0;
+      const animate = () => {
+        req = requestAnimationFrame(animate);
+        frame = frame <= 100 ? frame + 1 : frame;
+        if (frame <= 100) {
+          const p = initialCameraPosition;
+          const rotaionspeed = -easeOutCirc(frame / 120) * Math.PI * 20;
+
+          camera.position.y = 10;
+          camera.position.x =
+            p.x * Math.cos(rotaionspeed) * p.z * Math.sin(rotaionspeed);
+          camera.position.z =
+            p.z * Math.sin(rotaionspeed) - p.x * Math.cos(rotaionspeed);
+          camera.lookAt(target);
+        } else {
+          controls.update();
+        }
+
+        renderer.render(scene, camera);
+      };
+      return () => {
+        console.log("unmount");
+        cancelAnimationFrame(req);
+        renderer.dispose();
+      };
+    }
+  }, []);
+
+  return (
+    <div ref={refBody} style={{ width: "100%", height: "100%" }}>
+      {loading && <div>loading</div>}
+    </div>
+  );
+};
+
+export default Computer;
--- pages/Home.js
+++ pages/Home.js
@@ -1,79 +1,86 @@
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import { Button, Spacer, Grid } from "@nextui-org/react";
+import ComputerCanvas from "./Computer";
 
-function handleEntailmentRequest(e) {
-  // e.preventDefault();
-  //open link on the same page
-  // window.open("/education");
-}
 export default function Home() {
   return (
-    <div className="Homeintro" data-aos="fade-up" data-aos-duration="1000">
-      <h1 className="Homeh1">Hello 👋.</h1>
-      <h3 className="Homeh3">
-        I'm <span style={{ color: "#3bfff5" }}>Ibraheem ur Rehman</span> 👨‍💻
-        Software Engineer, Footballer ⚽, Gamer 🎮 Always Learning 📖.
-      </h3>
-      <Spacer x={1.5} />
-      <Grid.Container gap={2}>
-        <Spacer x={0.5} />
-        <Button
-          auto
-          css={{ backgroundColor: "#e4405f" }}
-          icon={
-            <img src="https://img.icons8.com/ios/30/000000/instagram-new.png" />
-          }
-          onClick={() =>
-            window.open("https://www.instagram.com/ibraheem_rehman/")
-          }
-        />
-        <Spacer x={0.5} />
-        <Button
-          auto
-          css={{ backgroundColor: "#1da1f2" }}
-          color="#1da1f2"
-          icon={
-            <img src="https://img.icons8.com/ios-filled/30/000000/twitter.png" />
-          }
-          onClick={() => window.open("https://twitter.com/ibraheem_rehman")}
-        />
-        <Spacer x={0.5} />
-        <Button
-          auto
-          css={{ backgroundColor: "#ffff" }}
-          icon={
-            <img src="https://img.icons8.com/ios-filled/30/000000/github.png" />
-          }
-          onClick={() => window.open("https://github.com/ibraheem15")}
-        />
-        <Spacer x={0.5} />
-        <Button
-          auto
-          css={{ backgroundColor: "#0e76a8" }}
-          icon={<img src="https://img.icons8.com/ios-filled/30/linkedin.png" />}
-          onClick={() =>
-            window.open(
-              "https://www.linkedin.com/in/m-ibraheem-rehman-qazi-711752244/"
-            )
-          }
-        />
-        <Spacer x={0.5} />
-        <Button
-          auto
-          css={{ backgroundColor: "#ea4335" }}
-          //gmail
-          icon={
-            <img src="https://img.icons8.com/ios-filled/30/000000/gmail.png" />
-          }
-          onClick={() => window.open("mail.google.com")}
-          // icon={<HeartIcon fill="currentColor" filled />} https://icons8.com/icon/62856/github
-        />
-      </Grid.Container>
-      <Spacer y={1.5} />
-      <Button size={"lg"} color="success" ghost>
-        <Link className="link_contactme" href="/education">Contact Me</Link>
-      </Button>
-    </div>
+    <section className="FirstView" data-aos="fade-up" data-aos-duration="1000">
+      <div className="Homeintro">
+        <h1 className="Homeh1">Hello 👋.</h1>
+        {/* <h1 className="Homeh1"></h1> */}
+        {/* <h1 className="Homeh1"></h1> */}
+        <h3 className="Homeh3">
+          I'm <span style={{ color: "#3bfff5" }}>Ibraheem ur Rehman</span> 👨‍💻
+          Software Engineer, Footballer ⚽, Gamer 🎮 Always Learning 📖.
+        </h3>
+        <Spacer x={1.5} />
+        <Grid.Container gap={2}>
+          <Spacer x={0.5} />
+          <Button
+            auto
+            css={{ backgroundColor: "#e4405f" }}
+            icon={
+              <img src="https://img.icons8.com/ios/30/000000/instagram-new.png" />
+            }
+            onClick={() =>
+              window.open("https://www.instagram.com/ibraheem_rehman/")
+            }
+          />
+          <Spacer x={0.5} />
+          <Button
+            auto
+            css={{ backgroundColor: "#1da1f2" }}
+            color="#1da1f2"
+            icon={
+              <img src="https://img.icons8.com/ios-filled/30/000000/twitter.png" />
+            }
+            onClick={() => window.open("https://twitter.com/ibraheem_rehman")}
+          />
+          <Spacer x={0.5} />
+          <Button
+            auto
+            css={{ backgroundColor: "#ffff" }}
+            icon={
+              <img src="https://img.icons8.com/ios-filled/30/000000/github.png" />
+            }
+            onClick={() => window.open("https://github.com/ibraheem15")}
+          />
+          <Spacer x={0.5} />
+          <Button
+            auto
+            css={{ backgroundColor: "#0e76a8" }}
+            icon={
+              <img src="https://img.icons8.com/ios-filled/30/linkedin.png" />
+            }
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/m-ibraheem-rehman-qazi-711752244/"
+              )
+            }
+          />
+          <Spacer x={0.5} />
+          <Button
+            auto
+            css={{ backgroundColor: "#ea4335" }}
+            //gmail
+            icon={
+              <img src="https://img.icons8.com/ios-filled/30/000000/gmail.png" />
+            }
+            onClick={() => window.open("mail.google.com")}
+            // icon={<HeartIcon fill="currentColor" filled />} https://icons8.com/icon/62856/github
+          />
+        </Grid.Container>
+        <Spacer y={1.5} />
+        <Button size={"lg"} color="success" ghost>
+          <Link className="link_contactme" href="/education">
+            Contact Me
+          </Link>
+        </Button>
+      </div>
+      <div className="computer">
+        <ComputerCanvas />
+      </div>
+    </section>
   );
 }
