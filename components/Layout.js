14db0a59f80f9b6279922e8e2a9fcff2477d2066
ibraheem15 <ibraheemrehman15@gmail.com>
2024-01-10 19:33:32 +0500

    Add Navbar component to up Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
+import Navbar from "../components/Navbar";
 import Footer from "./Footer";
 import Aos from "aos";
-import Navbar from "./Navbar";
 
 function Layout({ children }) {
   useEffect(() => {
